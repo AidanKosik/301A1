a6bcef16ad15882407d7bc60be291339df745300
AidanKosik <aidankosik@mac.com>
2018-01-10 12:43:00 -0700

    added application files

--- app/src/main/java/com/example/aidankosik/assignment1/MainActivity.java
+++ app/src/main/java/com/example/aidankosik/assignment1/MainActivity.java
@@ -8,8 +8,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ListView;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    public Button b_add, b_delete;
+    public ListView c_subscriptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +25,29 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Button b_add = (Button) findViewById(R.id.add);
+        Button b_delete = (Button) findViewById(R.id.delete);
+
+        b_add.setText("Add");
+        b_delete.setText("Delete");
+
+        ListView c_subscriptions = (ListView) findViewById(R.id.subs);
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+//        b_add.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast .makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
         return true;
     }
 
